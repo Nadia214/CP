ca4ea1d3c6952bc036f4705dbdd0130fea0ac068
nadia <47458652+Nadia214@users.noreply.github.com>
2022-04-05 23:17:28 +0600

    adding files

---
+++ Leetcode/Array/169. Majority Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int winner = 0;
+        int vote_count = 0;
+        for (auto num : nums){
+            if(vote_count == 0)
+                winner = num;
+
+            if(num == winner)
+                vote_count++;
+            else
+                vote_count--;
+        }
+
+      return winner;
+    }
+};
