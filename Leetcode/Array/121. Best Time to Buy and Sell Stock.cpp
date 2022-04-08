96d384d8c1a92833d28494f84f0b3d9a53aa99a0
nadia <47458652+Nadia214@users.noreply.github.com>
2022-04-09 00:00:03 +0600

    adding files

---
+++ Leetcode/Array/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int max_prof = 0;
+       int min_price = prices[0];
+
+       for(auto nums: prices){
+           min_price = min(min_price,nums);
+           int profit = nums - min_price;
+           max_prof = max(max_prof , profit);
+       }
+
+        return max_prof;
+    }
+};
